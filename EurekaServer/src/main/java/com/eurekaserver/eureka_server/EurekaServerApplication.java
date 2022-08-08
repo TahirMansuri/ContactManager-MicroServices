48938fc2b6a5a296a802ebad985f9b52d8cc9572
TahirMansuri <tprocks786@gmail.com>
2022-08-08 21:30:44 +0530

    ADDED: Eureka Server Offline Setup

--- EurekaServer/src/main/java/com/eurekaserver/eureka_server/EurekaServerApplication.java
+++ EurekaServer/src/main/java/com/eurekaserver/eureka_server/EurekaServerApplication.java
@@ -2,8 +2,13 @@ package com.eurekaserver.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/***
+ * @EnableEurekaServer annotation for Defining Service as Eureka Service
+ */
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
